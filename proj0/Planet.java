af127442c994b7f2cefdc64d051a25d4796f4cf7
Tong Yin <Tong Yin>
2017-01-28 02:23:30 -0600

    netForce methods and update position method added

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -31,14 +31,64 @@ public class Planet {
     double dy = p.yyPos - this.yyPos;
     return Math.sqrt(dx*dx+dy*dy);
   }
-  /** method return force exerted by another planet in x direction
-  public double calcForceExertedByX(Planet p){
-    return 0.0;
+
+  /** returns a force exerted on this planet by the given planet*/
+  public double calcForceExertedBy(Planet p){
+    double G = 6.67*Math.pow(10, -11);
+    double r = this.calcDistance(p);
+    return G*this.mass*p.mass/(r*r);
   }
 
-  /** method returns force exerted by another planet in y direction
+  /** method return force exerted by another planet in x direction*/
+  public double calcForceExertedByX(Planet p){
+    double F = this.calcForceExertedBy(p);
+    double dx = p.xxPos - this.xxPos;
+    double r = this.calcDistance(p);
+    return F*dx/r;
+  }
+
+  /** method returns force exerted by another planet in y direction*/
   public double calcForceExertedByY(Planet p){
-    return 0.0;
-  }*/
+    double F = this.calcForceExertedBy(p);
+    double dy = p.yyPos - this.yyPos;
+    double r = this.calcDistance(p);
+    return F*dy/r;
+  }
+
+  /** return net X force exerted by all planets in the array input*/
+  public double calcNetForceExertedByX(Planet[] ps){
+    int i = 0;
+    double netX = 0.0;
+    while(i < ps.length){
+      if(this.equals(ps[i]) == false){
+        netX = netX + this.calcForceExertedByX(ps[i]);
+      }
+      i=i+1;
+    }
+    return netX;
+  }
+
+  /** return net Y force exerted by all planets in the array input*/
+  public double calcNetForceExertedByY(Planet[] ps){
+    int i = 0;
+    double netY = 0.0;
+    while(i < ps.length){
+      if(this.equals(ps[i]) == false){
+        netY = netY + this.calcForceExertedByY(ps[i]);
+      }
+      i = i+1;
+    }
+    return netY;
+  }
+
+  /** update the planet position and velocitys*/
+  public void update(double dt, double netX, double netY){
+    double ax = netX/this.mass;
+    double ay = netY/this.mass;
+    xxVel = xxVel + dt*ax;
+    yyVel = yyVel + dt*ay;
+    xxPos = xxPos + dt*xxVel;
+    yyPos = yyPos + dt*yyVel;
+  }
 
 }
